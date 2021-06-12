1ba2d5bc2ffec1f7a16293039c0e3a8e5183879d
kaantpll <kaant43@gmail.com>
2021-06-12 11:12:38 +0300

    initial commit

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  static const String routeName = '/splash';
+
+  static Route route() {
+    return MaterialPageRoute(
+        settings: const RouteSettings(name: routeName),
+        builder: (_) => SplashScreen());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
